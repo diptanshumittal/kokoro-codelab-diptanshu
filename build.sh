e9b9cdc81d723be3c3cdeaddd3402f6b90110e77
diptanshumittal <diptanshu@google.com>
2023-04-18 17:39:23 +0530

    Build.sh update

--- build.sh
+++ build.sh
@@ -13,10 +13,9 @@ set -e
 # set -x
 curl -L \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer <YOUR-TOKEN>"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/googleapis/google-cloud-go/pulls/7462/files
-
+awk
 if [ "$1" == "release" ]; then
   javac -g:none Hello.java
 else
