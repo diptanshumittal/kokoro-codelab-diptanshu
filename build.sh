c51513cada85955a5e80ed9413683c78635020a6
diptanshumittal <diptanshu@google.com>
2023-04-19 03:12:46 +0530

    Updated build

--- build.sh
+++ build.sh
@@ -5,6 +5,13 @@ curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-
  jq -r 'map(select(.filename == ".release-please-manifest-individual.json" or .filename == ".release-please-manifest-submodules.json") | .patch)[0]' | 
  awk '/^[+]/{print substr($2,2, length($2)-3)}'
 
+read -a array <<< $(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" \
+ https://api.github.com/repos/googleapis/google-cloud-go/pulls/$PR_NUMBER/files | \
+ jq -r 'map(select(.filename == ".release-please-manifest-individual.json" or .filename == ".release-please-manifest-submodules.json") | .patch)[0]' | \
+ awk '/^[+]/{print substr($2,2, length($2)-3)}')
+
+echo ${#array[@]} 
+
 if [ "$1" == "release" ]; then
   javac -g:none Hello.java
 else
