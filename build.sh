749b82e287450b5f0d0e0619883dde88d007a156
diptanshumittal <diptanshu@google.com>
2023-04-19 02:57:36 +0530

    Updated build

--- build.sh
+++ build.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-# Fail on any error.
-set -e
-
-# Display commands being run.
-# WARNING: please only enable 'set -x' if necessary for debugging, and be very
-#  careful if you handle credentials (e.g. from Keystore) with 'set -x':
-#  statements like "export VAR=$(cat /tmp/keystore/credentials)" will result in
-#  the credentials being printed in build logs.
-#  Additionally, recursive invocation with credentials as command-line
-#  parameters, will print the full command, with credentials, in the build logs.
-# set -x
-curl -L \
-  -H "Accept: application/vnd.github+json" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/googleapis/google-cloud-go/pulls/7462/files
-awk -h
-jq -h
+curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" \
+ https://api.github.com/repos/googleapis/google-cloud-go/pulls/7687/files | \
+ jq -r 'map(select(.filename == ".release-please-manifest-individual.json" or .filename == ".release-please-manifest-submodules.json") | .patch)[0]'
 
 if [ "$1" == "release" ]; then
   javac -g:none Hello.java
