8105f2dcd83831531bcf8d9f0554117ad488a4c9
diptanshumittal <diptanshu@google.com>
2023-04-19 10:47:57 +0530

    Updated build

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ sty=$(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version:
  jq -r 'map(select(.filename == ".release-please-manifest-individual.json" or .filename == ".release-please-manifest-submodules.json") | .patch)[0]' | 
  awk '/^[+]/{print substr($2,2, length($2)-3)}')
 
- echo $sty
+echo $sty
 
 read -a array <<< $(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" \
  https://api.github.com/repos/googleapis/google-cloud-go/pulls/7687/files | \
