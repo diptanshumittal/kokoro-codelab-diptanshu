fd1612f144ce04a5fd51e212d8e142491abe335d
diptanshumittal <diptanshu@google.com>
2023-04-19 10:52:52 +0530

    Updated build

--- build.sh
+++ build.sh
@@ -7,12 +7,12 @@ sty=$(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version:
 
 echo $sty
 
-read -a array <<< $(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" \
+products=($(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" \
  https://api.github.com/repos/googleapis/google-cloud-go/pulls/7687/files | \
  jq -r 'map(select(.filename == ".release-please-manifest-individual.json" or .filename == ".release-please-manifest-submodules.json") | .patch)[0]' | \
- awk '/^[+]/{print substr($2,2, length($2)-3)}')
+ awk '/^[+]/{print substr($2,2, length($2)-3)}'))
 
-echo $array
+echo $products
 
 if [ "$1" == "release" ]; then
   javac -g:none Hello.java
