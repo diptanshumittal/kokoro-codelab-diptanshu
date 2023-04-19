dfc82cc56efe991df1f1d1a455ef109a7061a444
diptanshumittal <diptanshu@google.com>
2023-04-19 10:55:08 +0530

    Updated build

--- build.sh
+++ build.sh
@@ -7,10 +7,7 @@ sty=$(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version:
 
 echo $sty
 
-products=($(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" \
- https://api.github.com/repos/googleapis/google-cloud-go/pulls/7687/files | \
- jq -r 'map(select(.filename == ".release-please-manifest-individual.json" or .filename == ".release-please-manifest-submodules.json") | .patch)[0]' | \
- awk '/^[+]/{print substr($2,2, length($2)-3)}'))
+products=($sty)
 
 echo $products
 
