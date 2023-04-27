1968a452a123b5db5dddf548d55dab53b28f69be
diptanshumittal <diptanshu@google.com>
2023-04-27 22:04:16 +0530

    fdsfdfs

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo $KOKORO_PYTHON_COMMAND
+which python3
+which python3
 pip install -r requirements.txt
 
 sty=$(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" \
