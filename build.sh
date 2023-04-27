5d21abfea0a1da9526223d4048262e4d2b63aa7c
diptanshumittal <diptanshu@google.com>
2023-04-27 16:20:35 +0530

    fdsfdfs

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install -r requirements
+pip install -r requirements.txt
 
 sty=$(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" \
  https://api.github.com/repos/googleapis/google-cloud-go/pulls/7687/files | \
